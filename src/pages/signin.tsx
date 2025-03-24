0ef4d5b9c5e1b4aa4970f3f7b408a7a028074345
Chakit Bhandari <joshua@filmneverdie.com>
2025-03-24 14:31:18 +1100

    fix: remove password min length validation

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -70,10 +70,6 @@ export default function SignInForm() {
                   placeholder="Password"
                   {...register("password", {
                     required: "Password is required",
-                    minLength: {
-                      value: 8,
-                      message: "Password must be at least 8 characters",
-                    },
                   })}
                 />
                 <div onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
